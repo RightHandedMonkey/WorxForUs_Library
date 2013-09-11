b11369bf1e5f051cb49e44bb9ced069dee37545e
RightHandedMonkey <rooster@richcap.com>
2013-09-11 16:20:03 -0400

    Moved from com.worxforus db framework project

---
+++ src/com/worxforus/Constants.java
@@ -0,0 +1,12 @@
+package com.worxforus;
+
+public class Constants {
+
+	public static final String APK_CODE = "1";
+
+	// Meta Status Definitions
+	public static final int META_STATUS_NORMAL = 0;
+	public static final int META_STATUS_DELETED = 1;
+	public static final int META_STATUS_TEMPORARY = 2;
+
+}
